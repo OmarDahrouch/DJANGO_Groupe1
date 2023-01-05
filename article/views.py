0d5a0bdb982261b31aec2f1ff4ba4017eaf957bd
OmarDahrouch <idahrouch2@gmail.com>
2023-01-05 20:41:35 +0100

    Third

--- article/views.py
+++ article/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def hello(request):
+    # Do something here...
+    return HttpResponse("Hello, world!")
