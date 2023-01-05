6fcf26709d7e6969e736ca6fa236ba8a3883ec29
Mehdi Machane <mehdimachane2@gmail.com>
2023-01-05 23:27:36 +0100

    signup

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            # messages.success(request,f'Le compte de {username} à été bien crée')
+            return redirect('article')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
