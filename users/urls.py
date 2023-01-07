02471ae64751f3bf97a60ad2a9ad5f54676382da
Mehdi Machane <mehdimachane2@gmail.com>
2023-01-07 14:17:28 +0100

    login

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as login_views
+
+app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login"),
+    path('logout/', login_views.LogoutView.as_view(), name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from .forms import UserRegisterForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             # messages.success(request,f'Le compte de {username} à été bien crée')
-            return redirect('article')
+            return redirect('index')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/signup.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log the user in
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
