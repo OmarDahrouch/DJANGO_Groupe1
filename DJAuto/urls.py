57e973e1bd613cdcb96661c6f8c88f5f969bfd80
Mehdi Machane <mehdimachane2@gmail.com>
2023-01-07 15:52:22 +0100

    merge 2

--- DJAuto/urls.py
+++ DJAuto/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('users/', include("users.urls")),
 ]
 
+# contraint for media files
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
