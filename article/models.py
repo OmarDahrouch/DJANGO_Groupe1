4fc5009a278aae48f1f8c6980e5056a825e388af
OmarDahrouch <idahrouch2@gmail.com>
2023-01-08 04:02:26 +0100

    details of articles

--- article/models.py
+++ article/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Article(models.Model):
     nom_art = models.CharField(max_length=50, default=None)
     categorie = models.CharField(max_length=50, default=None)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=5000)
     ref_article = models.IntegerField()
     prix = models.FloatField()
     qte_stock = models.IntegerField()
