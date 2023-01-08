0234543d68ed5012dc38c7187b3525bfaa2e00e4
Mehdi Machane <mehdimachane2@gmail.com>
2023-01-08 17:58:53 +0100

    nav

--- DJAuto/settings.py
+++ DJAuto/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth', "crispy_forms",
     "crispy_bootstrap5", 'django.contrib.contenttypes',
     'django.contrib.sessions', 'django.contrib.messages',
-    'django.contrib.staticfiles', 'article', 'users'
+    'django.contrib.staticfiles', 'article', 'users', 'achat'
 ]
 
 MIDDLEWARE = [
