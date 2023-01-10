3fd3d6e7c2efcb906d1536a73e0e96479c5970bf
OmarDahrouch <idahrouch2@gmail.com>
2023-01-10 17:19:41 +0100

    Merge 10/01 17:00

--- article/urls.py
+++ article/urls.py
@@ -6,6 +6,6 @@ app_name = 'article'
 urlpatterns = [
     path('', views.home, name="home"),
     path('<int:myid>', views.detail, name="detail"),
-    path('checkout/', views.checkout, name="home"),
+    path('checkout/', views.checkout, name="checkout"),
     path('<str:cat>', views.categorie, name="cat")
 ]
