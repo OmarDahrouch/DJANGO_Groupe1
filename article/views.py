1dd54a486384a7ec89c3e3ce519b6963ddbe2971
OmarDahrouch <idahrouch2@gmail.com>
2023-01-15 02:10:12 +0100

    Merge finaaaaal

--- article/views.py
+++ article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article, Commande
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
